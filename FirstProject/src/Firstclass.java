c56c0ba58baeee7c1d606be84869af0edd0dfe11
Vajira <wajirakavindi@gmail.com>
2021-02-17 19:05:55 +0530

    initial commit

---
+++ FirstProject/src/Firstclass.java
@@ -0,0 +1,10 @@
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first project at SLIIT");
+
+	}
+
+}
